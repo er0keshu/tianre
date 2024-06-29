0c9870ad5b904e8fcad0314b5b025c6448285e20
zhangjun <meitt01@qq.com>
2024-06-29 15:15:08 +0800

    fix(app): 🐛  upload

--- server/api/upload.ts
+++ server/api/upload.ts
@@ -1,16 +1,27 @@
-import { writeFile } from 'node:fs';
+import { writeFile, createWriteStream } from 'node:fs'
+import { Readable } from 'node:stream'
 
 export default defineEventHandler(async (event) => {
-  const body = await readMultipartFormData(event);
+  const body = await readFormData(event)
 
-  const path = `${process.cwd()}/public/uploads/a.jpeg`;
+  const path = `${process.cwd()}/public/uploads/a.jpeg`
 
-  writeFile(path, body?.[1]?.data!, (err) => {
-    if (err) return console.log(err);
-    console.log('File uploaded successfully');
-  });
+  console.log((body.get('file') as File).arrayBuffer())
+
+  // writeFile(path, body?.[1]?.data!, (err) => {
+  //   if (err) return console.log(err)
+  //   console.log('File uploaded successfully')
+  // })
+
+  // const readable = new Readable({
+  //   read() {
+  //     this.push(body?.[1]?.data!)
+  //     this.push(null)
+  //   }
+  // })
+  // readable.pipe(createWriteStream(path))
 
   return {
-    name: body?.[0]?.data.toString(),
-  };
-});
+    // name: body?.[0]?.data.toString()
+  }
+})
